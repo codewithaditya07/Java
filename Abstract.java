e39337c7da188d9aaf54965d81a28fa3a6cc0c3c
codewithaditya07 <adityasahani680@gmail.com>
2025-04-03 23:05:41 +0530

    Java

--- Abstract.java
+++ Abstract.java
@@ -1,36 +1,64 @@
-abstract class Build
-{
-abstract void House(int a);
-}
+// abstract class Build
+// {
+// abstract void House(int a);
+// }
 
-class BD extends Build
-{
+// class BD extends Build
+// {
     
-void House(int a)
-{
-    System.out.println(a*a);
-}
+// void House(int a)
+// {
+//     System.out.println(a*a);
+// }
    
+// }
+
+// class T1 extends Build
+// {
+// void House(int a)
+// {
+//     System.out.println(a*a*a);
+// }
+// }
+
+// public class Abstract {
+
+//     public static void main(String[] args)
+//     {
+// BD ob=new BD();
+// ob.House(9);
+// T1 ob1=new T1();
+// ob1.House(8);
+
+
+//     }
+    
+// }
+
+
+// Abstract class
+abstract class Animal {
+    // Abstract method (does not have a body)
+    abstract void makeSound();
+
+    // Concrete method (has a body)
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
 }
 
-class T1 extends Build
-{
-void House(int a)
-{
-    System.out.println(a*a*a);
-}
+class Dog extends Animal {
+
+    void makeSound() {
+        System.out.println("Woof! Woof!");
+    }
 }
 
 public class Abstract {
-
-    public static void main(String[] args)
-    {
-BD ob=new BD();
-ob.House(9);
-T1 ob1=new T1();
-ob1.House(8);
-
-
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound(); 
+        myDog.sleep(); 
     }
-    
 }
+
