b92df79d94b6309d638aec6f3690c342dc0f7208
codewithaditya07 <adityasahani680@gmail.com>
2025-04-15 17:01:55 +0530

    Java

--- Abstract.java
+++ Abstract.java
@@ -1,64 +1,64 @@
-// abstract class Build
-// {
-// abstract void House(int a);
-// }
-
-// class BD extends Build
-// {
-    
-// void House(int a)
-// {
-//     System.out.println(a*a);
-// }
-   
-// }
-
-// class T1 extends Build
-// {
-// void House(int a)
-// {
-//     System.out.println(a*a*a);
-// }
-// }
-
-// public class Abstract {
-
-//     public static void main(String[] args)
-//     {
-// BD ob=new BD();
-// ob.House(9);
-// T1 ob1=new T1();
-// ob1.House(8);
-
-
-//     }
-    
-// }
-
-
-// Abstract class
-abstract class Animal {
-    // Abstract method (does not have a body)
-    abstract void makeSound();
-
-    // Concrete method (has a body)
-    void sleep() {
-        System.out.println("Sleeping...");
-    }
+abstract class Build
+{
+abstract void House(int a);
 }
 
-class Dog extends Animal {
+class BD extends Build
+{
+    
+void House(int a)
+{
+    System.out.println(a*a);
+}
+   
+}
 
-    void makeSound() {
-        System.out.println("Woof! Woof!");
-    }
+class T1 extends Build
+{
+void House(int a)
+{
+    System.out.println(a*a*a);
+}
 }
 
 public class Abstract {
-    public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.makeSound(); 
-        myDog.sleep(); 
+
+    public static void main(String[] args)
+    {
+BD ob=new BD();
+ob.House(9);
+T1 ob1=new T1();
+ob1.House(8);
+
+
     }
+    
 }
+
+
+// Abstract class
+// abstract class Animal {
+//     // Abstract method (does not have a body)
+//     abstract void makeSound();
+
+//     // Concrete method (has a body)
+//     void sleep() {
+//         System.out.println("Sleeping...");
+//     }
+// }
+
+// class Dog extends Animal {
+
+//     void makeSound() {
+//         System.out.println("Woof! Woof!");
+//     }
+// }
+
+// public class Abstract {
+//     public static void main(String[] args) {
+//         Dog myDog = new Dog();
+//         myDog.makeSound(); 
+//         myDog.sleep(); 
+//     }
+// }
 
---
+++ StringClass.java
@@ -0,0 +1,25 @@
+// // import util.java.Scanner;
+
+// public class StringClass {
+//     public static void main(String[] args) {
+//         String s= "hello world";
+// System.out.println(s);  
+// System.out.println("Length: " + s.length());
+// System.out.println("Char at 7: " + s.charAt(7));
+// System.out.println("Substring: " + s.substring(7));
+// System.out.println("Uppercase: " + s.toUpperCase());
+// System.out.println("Uppercase: " + s.toLowerCase());
+// System.out.println("Uppercase: " + s.indexOf("o"));    
+ 
+// String ss=new String("Hello Delhi");
+// System.out.println(ss);
+
+// if(s.equals(ss))
+//         System.out.println("Equal");
+// else
+//         System.out.println("Not Equal");
+
+
+// }
+// }
+        
