6197c063e3f8c408e269dc779650d0e8b499e8f9
codewithaditya07 <adityasahani680@gmail.com>
2025-04-16 22:15:17 +0530

    Java

--- StringClass.java
+++ StringClass.java
@@ -1,25 +1,24 @@
-// // import util.java.Scanner;
-
-// public class StringClass {
-//     public static void main(String[] args) {
-//         String s= "hello world";
-// System.out.println(s);  
-// System.out.println("Length: " + s.length());
-// System.out.println("Char at 7: " + s.charAt(7));
-// System.out.println("Substring: " + s.substring(7));
-// System.out.println("Uppercase: " + s.toUpperCase());
-// System.out.println("Uppercase: " + s.toLowerCase());
-// System.out.println("Uppercase: " + s.indexOf("o"));    
+public class StringClass {
+    public static void main(String[] args) {
+        String s= "hello world";
+System.out.println(s);  
+System.out.println("Length: " + s.length());
+System.out.println("Char at 7: " + s.charAt(7));
+System.out.println("Substring: " + s.substring(7));
+System.out.println("Uppercase: " + s.toUpperCase());
+System.out.println("Uppercase: " + s.toLowerCase());
+System.out.println("Uppercase: " + s.indexOf("o")); 
+System.out.println("Char at 3: " + s.charAt(3));   
  
-// String ss=new String("Hello Delhi");
-// System.out.println(ss);
+String ss=new String("Hello Delhi");
+System.out.println(ss);
 
-// if(s.equals(ss))
-//         System.out.println("Equal");
-// else
-//         System.out.println("Not Equal");
+if(s.equals(ss))
+        System.out.println("Equal");
+else
+        System.out.println("Not Equal");
 
 
-// }
-// }
+}
+}
         
